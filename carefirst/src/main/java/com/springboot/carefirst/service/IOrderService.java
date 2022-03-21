11ea9ae3bd478bc53d993adf053acd8fdf0f3359
aditya <aditya@10.66.0.140>
2022-03-21 12:21:49 +0000

    Order Service Interface

---
+++ carefirst/src/main/java/com/springboot/carefirst/service/IOrderService.java
@@ -0,0 +1,30 @@
+package com.springboot.carefirst.service;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.apache.tomcat.jni.Address;
+
+import com.springboot.carefirst.model.Client;
+import com.springboot.carefirst.model.Order;
+
+public interface IOrderService {
+
+	List<Order> getAll();
+
+	String addOrder(@Valid Order order);
+
+	String updateOrder(@Valid Order order);
+
+	String deleteOrder(Long orderID);
+
+	String getOrderDate(Long orderId);
+
+	String getShippedDate(Long orderId);
+
+	Address getOrderClientAddress(Long orderId);
+
+	Client getOrderClientInfo(Long orderId);
+
+}
