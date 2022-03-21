ce684272f790cda86a6ad9a4e0f5b473edb685f4
aditya <aditya@10.66.0.140>
2022-03-21 12:27:37 +0000

    ClientController

---
+++ carefirst/src/main/java/com/springboot/carefirst/controller/ClientController.java
@@ -0,0 +1,68 @@
+package com.springboot.carefirst.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.carefirst.model.Client;
+import com.springboot.carefirst.service.IClientService;
+
+
+
+@RestController // this class will be used as a controller
+public class ClientController {
+
+	@Autowired // no need to add confirmations or properties for this class
+	IClientService clientService; // reference of clientservice
+
+	@CrossOrigin
+	@GetMapping("/allClient")
+	public List<Client> getAllClients() {
+		System.out.println("Getting all data..");
+
+		return clientService.getAll();
+	}
+
+	@CrossOrigin
+	@GetMapping("/clientProfile")
+	public Client seeProfile(@RequestParam @Valid long clientid) {
+		return clientService.viewProfileInfo(clientid);
+	}
+
+	@CrossOrigin // transferring from web browser to application
+	@PostMapping("/registerClient") // create retrieve update and delete
+	public String addUser(@RequestBody @Valid Client client) {
+		String contact = clientService.register(client);
+		return contact;
+	}
+
+	@CrossOrigin
+	@DeleteMapping("/removeClient")
+	public String removeClient(@RequestParam @Valid long client) {
+		return clientService.removeClient(client);
+	}
+
+	@CrossOrigin
+	@PutMapping("/updateClientProfileInfo")
+	public String updateClient(@RequestParam @Valid long client, @RequestParam @Valid Object[] fields) {
+		return clientService.updateProfileInfo(client, fields);
+	}
+
+	@CrossOrigin
+	@PostMapping("/updateClient")
+	public String updateClient(@RequestBody @Valid Client client) {
+		String response = clientService.updateClient(client);
+		return response;
+	}
+
+}
